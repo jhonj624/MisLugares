38b53584da9a0c2c1ecb0c3f8fa1447a0082e6c6
Jhon Jairo Velásquez <jhonvelasquezudea@gmail.com>
2015-05-03 22:24:47 -0500

    Se enlazan las nuevas vistas agregadas

--- app/src/main/java/mislugares/example/jhon/misluguares/LugarMainActivity.java
+++ app/src/main/java/mislugares/example/jhon/misluguares/LugarMainActivity.java
@@ -1,12 +1,15 @@
 package mislugares.example.jhon.misluguares;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 /**
  * Created by jhon on 17/04/15.
@@ -37,6 +40,14 @@ public class LugarMainActivity extends ActionBarActivity {
             }
         });
 
+        Button bMostrarLugar = (Button) findViewById(R.id.mostrar_lugares);
+        bMostrarLugar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                lanzarVistaLugar(null);
+            }
+        });
+
         // listener para la actividad de acerca de ..
         bAcercaDe = (Button) findViewById(R.id.acercade);
         bAcercaDe.setOnClickListener(new View.OnClickListener() {
@@ -64,6 +75,11 @@ public class LugarMainActivity extends ActionBarActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            lanzarPreferencias(null);
+            return true;
+        }
+        if (id == R.id.action_ver_preferencias){
+            mostrarPreferencias();
             return true;
         }
         if(id == R.id.acerca_de){
@@ -71,7 +87,7 @@ public class LugarMainActivity extends ActionBarActivity {
             return true;
         }
         if(id == R.id.menu_buscar){
-            onCrearLugar(null);
+            lanzarVistaLugar(null);
             return true;
         }
 
@@ -86,6 +102,30 @@ public class LugarMainActivity extends ActionBarActivity {
 
     public void onCrearLugar(View view){
         Intent i = new Intent(this,EdicionLugarActivity.class);
+        startActivity(i);
+    }
+
+    public void lanzarPreferencias(View view){
+        Intent i = new Intent(this,Preferencias.class);
+        startActivity(i);
+    }
+
+    public void mostrarPreferencias(){
+        SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(this);
+        //int km =  pref.getInt("distancia",0);
+        //String distancia = Integer.toString(km);
+
+        String s ="notificaciones: "+pref.getBoolean("notificaciones",true)
+                + ", distancia mínima: "+pref.getString("distancia: ","?")
+                + ", orden: "+ pref.getString("orden","1");
+        Toast.makeText(this,s,Toast.LENGTH_SHORT).show();
+
+
+    }
+
+    public void lanzarVistaLugar(View view){
+        Intent i = new Intent(this, VistaLugar.class);
+        i.putExtra("id", (long)0);
         startActivity(i);
     }
 }
